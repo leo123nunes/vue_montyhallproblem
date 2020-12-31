84fd9f9994bd378d5bcda54a4e9f2d1f55405b52
leo123nunes <ti.userleo@hotmail.com>
2020-12-31 17:01:29 -0300

    Creating the gift with pure css

---
+++ src/components/Gift.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="gift">
+        <div class="gift-top"></div>
+        <div class="gift-tape1"></div>
+        <div class="gift-tape2"></div>
+        <div class="gift-body"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.gift{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.gift-top{
+    height: 20px;
+    width: 70px;
+    background-color: #13991e;
+}
+
+.gift-tape1{
+    width: 60px;
+    height: 10px;
+    position: absolute;
+    background-color: red;
+    margin-top: 35px;
+}
+
+.gift-tape2{
+    width: 10px;
+    height:70px;
+    position: absolute;
+    background-color: red;
+    margin-left: 0px;
+}
+
+.gift-body{
+    width: 60px;
+    height: 50px;
+    background-color: #0a5810;
+}
+</style>
